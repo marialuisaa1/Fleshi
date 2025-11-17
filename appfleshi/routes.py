31d0d1b18d155e263fd8fe9afa885ca69a4e7b5d
MariaLuisa <marialuisap92@gmail.com>
2025-11-17 16:26:54 -0300

    chore: creation of default files and folders

---
+++ appfleshi/routes.py
@@ -0,0 +1,11 @@
+from flask import render_template
+
+from app import app
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/profile/<username>')
+def profile(username):
+    return render_template('profile.html', username=username)
