d4dd08d67e50e416db6f2d3b7531abe25653987a
MaRia Luísa <marialuisap92@gmail.com>
2025-11-24 15:43:35 -0300

    feat: add class LoginForm and RegisterForm

---
+++ appfleshi/form.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length, equal_to
+from appfleshi.models import User
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    submit = SubmitField('Entrar')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired(), Length(min=6, max=60)])
+    confirm_password = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Criar Conta')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            return ValidationError("E-mail já cadastrado")
+        return None
