df6bc22b32ef24b5c8311a91d13db4d62e781c93
Maria Luísa <marialuisap92@gmail.com>
2025-12-08 15:33:20 -0300

    feat: add authentication validators

--- appfleshi/forms.py
+++ appfleshi/forms.py
@@ -13,6 +13,13 @@ class LoginForm(FlaskForm):
     password = PasswordField('Senha', validators=[DataRequired()])
     submit = SubmitField('Entrar')
 
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if not user:
+            raise ValidationError("Usuário não encontrado!")
+        return None
+
+
 class RegisterForm(FlaskForm):
     username = StringField('Nome de Usuário', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -20,8 +27,13 @@ class RegisterForm(FlaskForm):
     confirm_password = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Criar Conta')
 
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("E-mail já cadastrado")
+        return None
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            return ValidationError("E-mail já cadastrado")
+            raise ValidationError("Username já existe")
         return None
